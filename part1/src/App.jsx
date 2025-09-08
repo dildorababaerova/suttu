35b61461a5c1d35945cd2776ba118c08615a7088
Dildora <48212@edu.raseko.fi>
2025-09-08 15:13:24 +0300

    part1 exercise 1.7 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
+const Statistics = (props) => {
+  if (props.total === 0) {
     return (
       <div>
-        the app is used by pressing the buttons
+        No feedback given
       </div>
     )
   }
-
-  return (
+  return(
     <div>
-      button press history: {props.allClicks.join(' ')}
+     
+      <p>good:{props.good}</p>
+      <p>neutral:{props.neutral}</p>
+      <p>bad:{props.bad}</p>
+      <p>average:{props.average}</p>
+      <p>total:{props.total}</p>
+      <p>positive:{props.positive}%</p>
     </div>
   )
 }
@@ -21,29 +25,58 @@ const History = (props) => {
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const stat ={good, neutral, bad, total, average, positive}
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-    console.log(left)
+  const handleGoodClick = () => {
+    const goodFeedBack=good+1
+    setGood(goodFeedBack)
+    const totalFeedBack=goodFeedBack+neutral+bad 
+    setTotal(totalFeedBack)
+    const averageFeedBack=((goodFeedBack-bad)/totalFeedBack).toFixed(2)
+    setAverage(averageFeedBack)
+    const positiveFeedBack=(goodFeedBack/totalFeedBack*100).toFixed(2)
+    setPositive(positiveFeedBack)
+
+  }
+  
+  
+  const handleNeutralClick = () => {
+    const neutralFeedBack=neutral+1
+    setNeutral(neutralFeedBack)
+    const totalFeedBack=good+neutralFeedBack+bad 
+    setTotal(totalFeedBack)
+    const averageFeedBack=((good-bad)/totalFeedBack).toFixed(2)
+    setAverage(averageFeedBack)
+    const positiveFeedBack=(good/totalFeedBack*100).toFixed(2)
+    setPositive(positiveFeedBack)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const handleBadClick = () => {
+    const badFeedBack=bad+1
+    setBad(badFeedBack)
+    const totalFeedBack=good+neutral+badFeedBack 
+    setTotal(totalFeedBack)
+    const averageFeedBack=( (good-badFeedBack)/totalFeedBack).toFixed(2)
+    setAverage(averageFeedBack)
+    const positiveFeedBack=(good/totalFeedBack*100).toFixed(2)
+    setPositive(positiveFeedBack)
   }
 
   return (
     <div>
-      {left}
-
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <h2>give feedback</h2>
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+       <h2>statistics</h2>
+      <Statistics {...stat}
+      />
     </div>
   )
 }
