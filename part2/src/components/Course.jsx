d59eccb41fc3dbd7b18261992642bc03b9295181
Dildora <dildora.babaerova@gmail.com>
2025-09-11 10:49:03 +0300

    part2 exercise 2.5 done

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,22 +1,62 @@
-import Header from './Header.jsx';
-import Content from './Content.jsx'
-import Total from './Total.jsx'
+const Total = ({parts}) => {
 
-
-const Course = ({courses}) => {
-    
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
     return (
         <div>
-      {courses.map(course => (
-        <div key={course.id}>
-          <Header name={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
+            <strong>
+            Total of {total} exercises
+            </strong>
         </div>
-      ))}
-    </div>
-        
     )
+
+}
+
+
+const Part = ({part}) => {
+    return (
+        <div>
+            {part.name} {part.exercises}
+        </div>
+    )
+}
+
+const Content = ({parts}) => {
+    const content = parts.map(part =>
+    < Part key = {part.id} part={part}/>  
+    )
+    return (
+        <div>{content}</div>
+    )
+}
+
+
+const Header = ({name}) => {
+    return (
+        <div>
+            <h1>{name}</h1>
+        </div>
+    )
+}
+
+const Course = ({courses}) => {
+    console.log(courses[0].name);
+
+    return (
+
+        <div>
+            {courses.map(course =>
+            <div key={course.id}>
+
+                < Header  name={course.name}/>   
+                < Content parts={course.parts}/> 
+                < Total parts ={course.parts}/>  
+            </div>
+            )
+            }
+
+        </div>
+    )
+
 }
 
 export default Course
---
+++ part2/src/components/Course_Refactoring.jsx
@@ -0,0 +1,22 @@
+import Header from './Header.jsx';
+import Content from './Content.jsx'
+import Total from './Total.jsx'
+
+
+const Course = ({courses}) => {
+    
+    return (
+        <div>
+      {courses.map(course => (
+        <div key={course.id}>
+          <Header name={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      ))}
+    </div>
+        
+    )
+}
+
+export default Course
