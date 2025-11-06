ed6d038145e603a6feb573163c6c4cad60a5cd4c
Dildora <48212@edu.raseko.fi>
2025-11-06 23:05:14 +0200

    changed config

--- part2/vite.config.js
+++ part2/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy: { 
       '/api':
-      {target: 'http://localhost/api/persons',
+      {target: 'http://localhost:3001',
         changeOrigin:true,
       },
     }
