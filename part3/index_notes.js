3eb20d3fbf6cfacc5fa4938af160630c179809be
Dildora <48212@edu.raseko.fi>
2025-11-08 07:09:52 +0200

    uibuild

--- part3/index_notes.js
+++ part3/index_notes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(express.json())
+const morgan = require('morgan')
 app.use(cors())
 app.use(express.static('dist'))
 
