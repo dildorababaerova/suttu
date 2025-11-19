1ce5b37945510e569b0e9a2685861158ea0757af
Dildora <48212@edu.raseko.fi>
2025-11-19 15:45:20 +0200

    exercises 4.1-4.2 done

---
+++ part3/tests/reverse.test.js
@@ -0,0 +1,22 @@
+const {test} = require('node:test')
+const assert = require('node:assert')
+
+const reverse = require('../utils/for_testing').reverse
+
+test('reverse of sagdiana', () => {
+    const result = reverse('sagdiana')
+    
+    assert.strictEqual(result, 'anaidgas')
+})
+
+test('reverse of dildora', () => {
+    const result = reverse('dildora')
+    
+    assert.strictEqual(result, 'arodlid')
+})
+
+test('reverse of jasur', () => {
+    const result = reverse('jasur')
+    
+    assert.strictEqual(result, 'rusaj')
+})
